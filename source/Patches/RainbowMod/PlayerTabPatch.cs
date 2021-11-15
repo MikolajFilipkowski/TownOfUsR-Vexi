b5d164c5f8e1646306a99def697e756aa037d489
OhMyGuus <info@guus.ninja>
2021-11-15 14:44:05 +0100

    Fix colortab

--- source/Patches/RainbowMod/PlayerTabPatch.cs
+++ source/Patches/RainbowMod/PlayerTabPatch.cs
@@ -6,35 +6,31 @@ using Object = UnityEngine.Object;
 
 namespace TownOfUs.RainbowMod
 {
-    //TODO: LOOK INTO THIS
-    //[HarmonyPatch(typeof(PlayerTab), nameof(PlayerTab.OnEnable))]
-    //public class PlayerTabPatch
-    //{
-    //    public static bool Prefix(PlayerTab __instance)
-    //    {
-    //        PlayerControl.SetPlayerMaterialColors(PlayerControl.LocalPlayer.DefaultOutfit.ColorId, __instance.DemoImage);
-    //        __instance.HatImage.SetHat(SaveManager.LastHat, PlayerControl.LocalPlayer.DefaultOutfit.ColorId);
-    //        PlayerControl.SetSkinImage(SaveManager.LastSkin, __instance.SkinImage);
-    //        PlayerControl.SetPetImage(SaveManager.LastPet, PlayerControl.LocalPlayer.DefaultOutfit.ColorId, __instance.PetImage);
-    //        var colors = Palette.PlayerColors;
-    //        var num = colors.Length / 4f;
-    //        for (int i = 0;i < colors.Length;i++)
-    //        {
-    //            var x = __instance.XRange.Lerp((i % 4) / 4f) + 0.25f;
-    //            var y = __instance.YStart - (i / 4) * 0.55f;
-    //            var colorChip = Object.Instantiate(__instance.ColorTabPrefab, __instance.ColorTabArea, true);
-    //            colorChip.transform.localScale *= 0.8f;
-    //            colorChip.transform.localPosition = new Vector3(x, y, -1f);
-    //            var colorId = (byte)i;
-    //            colorChip.Button.OnClick.AddListener((Action) (() =>
-    //            {
-    //                __instance.SelectColor(colorId);
-    //                if (colorId <= 17) SaveManager.BodyColor = colorId;
-    //            }));
-    //            colorChip.Inner.color = colors[i];
-    //            __instance.ColorChips.Add(colorChip);
-    //        }
-    //        return false;
-    //    }
-    //}
+    [HarmonyPatch(typeof(PlayerTab), nameof(PlayerTab.OnEnable))]
+    public class PlayerTabPatch
+    {
+        public static bool Prefix(PlayerTab __instance)
+        {
+            var colors = Palette.PlayerColors;
+            var num = colors.Length / 4f;
+            for (int i = 0; i < colors.Length; i++)
+            {
+                var x = __instance.XRange.Lerp((i % 4) / 4f) + 0.25f;
+                var y = __instance.YStart - (i / 4) * 0.55f;
+                var colorChip = Object.Instantiate(__instance.ColorTabPrefab, __instance.ColorTabArea, true);
+                colorChip.transform.localScale *= 0.8f;
+                colorChip.transform.localPosition = new Vector3(x, y, -1f);
+                var colorId = (byte)i;
+                colorChip.Button.OnClick.AddListener((Action)(() =>
+               {
+                   __instance.SelectColor(colorId);
+                   if (colorId <= 17) SaveManager.BodyColor = colorId;
+               }));
+                colorChip.Inner.color = colors[i];
+                __instance.ColorChips.Add(colorChip);
+            }
+            __instance.currentColor = (int)SaveManager.BodyColor;
+            return false;
+        }
+    }
 }
