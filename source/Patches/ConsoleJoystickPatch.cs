9228bce4b5a451258dc4d5a48a6f83f879090e8e
OhMyGuus <info@guus.ninja>
2021-11-20 08:45:50 +0100

    Fixed vent hotkey for non impostor roles.

---
+++ source/Patches/ConsoleJoystickPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TownOfUs.Patches
+{
+    [HarmonyPatch(typeof(ConsoleJoystick), nameof(ConsoleJoystick.HandleHUD))]
+    public static class ConsoleJoystickPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix()
+        {
+            if (DestroyableSingleton<HudManager>.Instance != null && DestroyableSingleton<HudManager>.Instance.ImpostorVentButton != null && DestroyableSingleton<HudManager>.Instance.isActiveAndEnabled && ConsoleJoystick.player.GetButtonDown(50))
+                DestroyableSingleton<HudManager>.Instance.ImpostorVentButton.DoClick();
+        }
+    }
+}
